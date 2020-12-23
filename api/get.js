7013216ff940e29206e934ed10896a7b07d3e151
PonomareVlad <Git@PonomareVlad.ru>
2020-12-23 10:35:35 +0500

    Reformat

--- api/get.js
+++ api/get.js
@@ -2,7 +2,8 @@ import connectDB, {client as mongo} from "../db";
 
 export default async (req, res) => {
     await connectDB()
-    const room = await mongo.db("chat").collection("queue").findOne()
-    if (room) await mongo.db("chat").collection("queue").deleteOne(room)
+    const queue = mongo.db("chat").collection("queue");
+    const room = await queue.findOne()
+    if (room) await queue.deleteOne(room)
     return res.json({room})
 }
