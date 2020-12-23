2de7dcec7ad001f20f6b75f096f41cd738371a4d
PonomareVlad <Git@PonomareVlad.ru>
2020-12-23 09:03:08 +0500

    Updated functions

--- api/get.js
+++ api/get.js
@@ -1,7 +1,3 @@
 import connectDB, {client as mongo} from "../db";
 
-export default async (req, res) => {
-    await connectDB()
-    const item = await mongo.db("chat").collection("queue").findOne()
-    return res.json({item})
-}
+export default async (req, res) => await connectDB() && res.json({room: await mongo.db("chat").collection("queue").findOne()})
--- api/insert.js
+++ api/insert.js
@@ -1,6 +1,7 @@
 import connectDB, {client as mongo} from "../db";
 
 export default async ({body} = req, res) => {
+    if (!body.id) return {status: false}
     await connectDB()
     const queue = mongo.db("chat").collection("queue");
     const status = await queue.findOne(body) || await queue.insertOne(body)
