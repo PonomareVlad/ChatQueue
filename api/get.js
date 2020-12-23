b4cb685877f07daee98ecebf057d390227da9e4b
PonomareVlad <Git@PonomareVlad.ru>
2020-12-23 10:33:51 +0500

    Remove room while getting

--- api/get.js
+++ api/get.js
@@ -3,6 +3,6 @@ import connectDB, {client as mongo} from "../db";
 export default async (req, res) => {
     await connectDB()
     const room = await mongo.db("chat").collection("queue").findOne()
-    await mongo.db("chat").collection("queue").deleteOne(room)
+    if (room) await mongo.db("chat").collection("queue").deleteOne(room)
     return res.json({room})
 }
