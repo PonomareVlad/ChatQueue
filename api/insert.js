18bd15e950ec6e453441c3f99ef972b18f26ef32
PonomareVlad <Git@PonomareVlad.ru>
2020-12-23 08:11:50 +0500

    Check if item exist while insertion

--- api/insert.js
+++ api/insert.js
@@ -2,6 +2,7 @@ import connectDB, {client as mongo} from "../db";
 
 export default async ({body} = req, res) => {
     await connectDB()
-    const status = await mongo.db("chat").collection("queue").insertOne(body)
+    const queue = mongo.db("chat").collection("queue");
+    const status = await queue.findOne(body) || await queue.insertOne(body)
     return res.json({status})
 }
