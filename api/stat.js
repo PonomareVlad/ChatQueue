b3c4f12114ab8a9d8f166677948acb911d6da418
PonomareVlad <Git@PonomareVlad.ru>
2020-12-24 23:10:10 +0500

    Statistic viewer

--- api/stat.js
+++ api/stat.js
@@ -2,6 +2,8 @@ import connectDB, {client as mongo} from "../db";
 
 export default async (req, res) => {
     await connectDB()
-    const rooms = await mongo.db("chat").collection("rooms").find();
+    const data = await mongo.db("chat").collection("rooms").find({});
+    let rooms = [];
+    await data.forEach(row => rooms.push(row))
     return res.json({rooms});
 }
